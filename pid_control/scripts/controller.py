fbe616a5c64d4c743da1dc8df6a41e6a95668b39
Karen-Rdz <karen.rdz.h@gmail.com>
2023-02-23 14:02:51 -0600

    Empezar PID

--- pid_control/scripts/controller.py
+++ pid_control/scripts/controller.py
@@ -6,20 +6,37 @@ from pid_control.msg import motor_input
 from pid_control.msg import set_point
 
 #Setup parameters, vriables and callback functions here (if required)
+setpoint = 0.0
+output = motor_input()
+
 def set_point_callback(msg):
-    rospy.loginfo("Setpoint: %s", msg.data)
-    rospy.get_param("Setpoint", "No param found")
+    rospy.loginfo("Setpoint: %s", msg.setpoint)
+
+    global setpoint
+    setpoint = msg.setpoint
 
 def motor_output_callback(msg):
     rospy.loginfo("Motor output: %s", msg.output)
+    
+    global setpoint
+    global output
+    Kp = rospy.get_param("Kp", "No param found")
+    e = setpoint - msg.output
+    output.input = Kp * e
+    output.time = rospy.get_time()
+
+    rospy.loginfo("Error: %s", e)
+    rospy.loginfo("Kp: %s", Kp)
+    rospy.loginfo("Motor input: %s", output)
 
 #Stop Condition
 def stop():
  #Setup the stop message (can be the same as the control message)
   print("Stopping")
 
-
 if __name__=='__main__':
+    print("The Controller is Running")
+
     #Initialise and Setup node
     rospy.init_node("controller")
     rate = rospy.Rate(100)
@@ -28,12 +45,11 @@ if __name__=='__main__':
     #Setup Publishers and subscribers here
     rospy.Subscriber("set_point", set_point, set_point_callback)
     rospy.Subscriber("motor_output", motor_output, motor_output_callback)
-    pub = rospy.Publisher("motor_input", motor_input , queue_size=1) 
+    input_pub = rospy.Publisher("motor_input", motor_input , queue_size=1) 
 
-    print("The Controller is Running")
     #Run the node
     while not rospy.is_shutdown():
 
         #Write your code here
-
+        input_pub.publish(output)
         rate.sleep()
--- pid_control/scripts/set_point_generator.py
+++ pid_control/scripts/set_point_generator.py
@@ -5,27 +5,30 @@ from pid_control.msg import set_point
 
 # Setup Variables, parameters and messages to be used (if required)
 
-
 #Stop Condition
 def stop():
  #Setup the stop message (can be the same as the control message)
   print("Stopping")
 
-
 if __name__=='__main__':
+    print("The Set Point Genertor is Running")
+
     #Initialise and Setup node
     rospy.init_node("Set_Point_Generator")
     rate = rospy.Rate(100)
     rospy.on_shutdown(stop)
 
     #Setup Publishers and subscribers here
+    setpoint_pub = rospy.Publisher("set_point", set_point , queue_size=1) 
 
-
-    print("The Set Point Genertor is Running")
+    # Set the message
+    msg = set_point()
+    msg.setpoint = 0.5
+    msg.time = rospy.get_time()
 
     #Run the node
     while not rospy.is_shutdown():
 
         #Write your code here
-
+        setpoint_pub.publish(msg)
         rate.sleep()
