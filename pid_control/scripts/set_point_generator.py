3f1803f07af68fc126eb6872ea431b9b91a7c96e
cesar121101 <A00829832@itesm.mx>
2023-02-24 18:35:58 -0600

    onda senoidal cuadrada

--- pid_control/scripts/set_point_generator.py
+++ pid_control/scripts/set_point_generator.py
@@ -30,7 +30,8 @@ if __name__=='__main__':
     while not rospy.is_shutdown():
         msg = set_point()
         #msg.setpoint = rospy.get_param("Setpoint", "No setpoint found")
-        msg.setpoint = np.sin(rospy.get_time()*0.25*np.pi)
+        #msg.setpoint = np.sin(rospy.get_time()*np.pi)
+        msg.setpoint = 5
         msg.time = rospy.get_time()
 
 		#Write your code here
