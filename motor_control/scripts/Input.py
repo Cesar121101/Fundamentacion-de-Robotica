4a285577b8e23dff0489b7267499e59242335e23
Karen-Rdz <karen.rdz.h@gmail.com>
2023-02-28 12:02:55 -0600

    agregar código input

--- motor_control/scripts/Input.py
+++ motor_control/scripts/Input.py
@@ -1,1 +1,32 @@
-# Write your code here
+#!/usr/bin/env python
+import rospy
+import numpy as np
+from std_msgs.msg import String
+from motor_control.msg import motor_input
+
+# Setup Variables, parameters and messages to be used
+
+#Stop Condition
+def stop():
+  print("Stopping")
+
+if __name__=='__main__':
+    print("The Input Genertor is Running")
+
+    #Initialise and Setup node
+    rospy.init_node("input")
+    rate = rospy.Rate(100)
+    rospy.on_shutdown(stop)
+
+    #Setup Publisher
+    pwm_pub = rospy.Publisher("cmd_pwm", motor_input , queue_size=1) 
+
+	#Run the node
+    while not rospy.is_shutdown():
+        msg = motor_input()
+        msg.input = np.sin(rospy.get_time()*0.8)*4
+        msg.time = rospy.get_time()
+
+		#Write your code here
+        pwm_pub.publish(msg)
+        rate.sleep()
