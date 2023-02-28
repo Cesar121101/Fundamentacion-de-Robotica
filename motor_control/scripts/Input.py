9e2f951a51d9f34883daa5109cd903225fabcf97
Karen-Rdz <karen.rdz.h@gmail.com>
2023-02-28 15:03:08 -0600

    cambio en input

--- motor_control/scripts/Input.py
+++ motor_control/scripts/Input.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import rospy
 import numpy as np
-from std_msgs.msg import String
+from std_msgs.msg import Float32
 from motor_control.msg import motor_input
 
 # Setup Variables, parameters and messages to be used
@@ -19,14 +19,17 @@ if __name__=='__main__':
     rospy.on_shutdown(stop)
 
     #Setup Publisher
-    pwm_pub = rospy.Publisher("cmd_pwm", motor_input , queue_size=1) 
+    pwm_pub = rospy.Publisher("cmd_pwm", Float32 , queue_size=1) 
 
 	#Run the node
     while not rospy.is_shutdown():
-        msg = motor_input()
-        msg.input = np.sin(rospy.get_time()*0.8)*4
-        msg.time = rospy.get_time()
+        # Custom message
+        #msg = motor_input()
+        #msg.input = np.sin(rospy.get_time()*0.8)*4
+        #msg.time = rospy.get_time()
 
-		#Write your code here
+        msg = np.sin(rospy.get_time()*0.8)*4
+
+		# Publish message
         pwm_pub.publish(msg)
         rate.sleep()
