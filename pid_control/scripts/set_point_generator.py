126464f47d9ae9a0da46b1d2dbaf2ccabf2ae69e
cesar121101 <A00829832@itesm.mx>
2023-02-27 18:50:05 -0600

    ejercicio terminado

--- pid_control/scripts/set_point_generator.py
+++ pid_control/scripts/set_point_generator.py
@@ -35,7 +35,7 @@ if __name__=='__main__':
     while not rospy.is_shutdown():
         msg = set_point()
         #msg.setpoint = rospy.get_param("Setpoint", "No setpoint found")
-        #msg.setpoint = np.sin(rospy.get_time()*0.25*np.pi)
+        #msg.setpoint = np.sin(rospy.get_time()*0.8)*4
         if(rospy.get_time() - previoustime >= 5):
             if(flag == 1): 
                 valoractual = 5.0
