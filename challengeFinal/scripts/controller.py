23180084f9e265d178524fec142bf6967fbcdc1b
Karen-Rdz <karen.rdz.h@gmail.com>
2023-03-07 17:57:52 -0600

    configurar paquete challengeFInal

---
+++ challengeFinal/scripts/controller.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+from challengeFinal.msg import motor_output
+from challengeFinal.msg import motor_input
+from challengeFinal.msg import set_point
+from std_msgs.msg import Float32
+
+#Setup parameters, vriables and callback functions here (if required)
+setpoint = 0.0
+out = motor_input()
+superError = 0.0
+currentTime = 0.0
+prevTime = 0.0
+prevError = 0.0
+errorGlob = 0.0
+motorOut = motor_output()
+
+def PID(error):
+    global currentTime
+    global prevTime
+    global superError
+    global prevError
+
+    dt = currentTime-prevTime
+    
+    # P
+    Kp = rospy.get_param("Kp", "No param found")
+    P = Kp*error
+
+    # I
+    superError += error * dt
+    Ki = rospy.get_param("Ki", "NO param found")
+    I = superError*Ki
+
+    # D
+    Kd = rospy.get_param("Kd", "NO param found")
+    D = Kd*((error-prevError)/dt)
+
+    prevError = error
+
+    return (P + I + D)
+    
+
+def set_point_callback(msg):
+    rospy.loginfo("Setpoint: %s", msg.setpoint)
+
+    global setpoint
+    setpoint = msg.setpoint
+
+def motor_output_callback(msg):
+    global motor_output
+    motorOut.output = msg.output
+    motorOut.time = msg.time
+    motorOut.status = msg.status
+
+#Stop Condition
+def stop():
+ #Setup the stop message (can be the same as the control message)
+  print("Stopping")
+
+if __name__=='__main__':
+    print("The Controller is Running")
+
+    #Initialise and Setup node
+    rospy.init_node("controller")
+    rate = rospy.Rate(100)
+    rospy.on_shutdown(stop)
+
+    #Setup Publishers and subscribers here
+    rospy.Subscriber("set_point", set_point, set_point_callback)
+    rospy.Subscriber("motor_output", motor_output, motor_output_callback)
+    input_pub = rospy.Publisher("motor_input", motor_input , queue_size=1)
+    error_pub = rospy.Publisher("error", Float32 , queue_size=1) 
+
+    #Set the current time
+    currentTime = rospy.get_time()
+
+    #Run the node
+    while not rospy.is_shutdown():
+        prevTime = currentTime
+        currentTime = rospy.get_time()
+        error = setpoint - motorOut.output
+        out.input = PID(error)
+        out.time = motorOut.time
+
+        rospy.loginfo("Motor output: %s", motorOut.output)
+        rospy.loginfo("Error: %s", error)
+        rospy.loginfo("Motor input: %s", out)
+        
+        if(out.input > 1):
+            out.input = 1
+        elif(out.input < -1):
+            out.input = -1
+
+        #Write your code here
+        error_pub.publish(error)
+        input_pub.publish(out)
+        rate.sleep()
---
+++ challengeFinal/scripts/set_point_generator.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+from challengeFinal.msg import set_point
+
+# Setup Variables, parameters and messages to be used (if required)
+
+#Stop Condition
+def stop():
+ #Setup the stop message (can be the same as the control message)
+  print("Stopping")
+
+if __name__=='__main__':
+    print("The Set Point Genertor is Running")
+
+    #Initialise and Setup node
+    rospy.init_node("Set_Point_Generator")
+    rate = rospy.Rate(100)
+    rospy.on_shutdown(stop)
+
+    #Setup Publishers and subscribers here
+    setpoint_pub = rospy.Publisher("set_point", set_point , queue_size=1) 
+
+    # Set the message
+    msg = set_point()
+    msg.setpoint = 0.0
+    msg.time = rospy.get_time()
+
+    #Set previous time 
+    previoustime = rospy.get_time()
+    flag = 1
+    valoractual = 0.0
+
+	#Run the node
+    while not rospy.is_shutdown():
+        msg = set_point()
+        #msg.setpoint = rospy.get_param("Setpoint", "No setpoint found")
+        #msg.setpoint = np.sin(rospy.get_time()*0.8)*4
+        if(rospy.get_time() - previoustime >= 5):
+            if(flag == 1): 
+                valoractual = 5.0
+                flag = 0
+            elif(flag == 0):
+                valoractual = -5.0
+                flag = 1
+            previoustime = rospy.get_time()
+        msg.setpoint = valoractual
+        msg.time = rospy.get_time()
+		#Write your code here
+        setpoint_pub.publish(msg)
+        rate.sleep()
