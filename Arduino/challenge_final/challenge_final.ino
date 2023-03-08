d2b059044fc0e0591a2ebeb5bb8076f30456327f
Karen-Rdz <karen.rdz.h@gmail.com>
2023-03-07 18:30:02 -0600

    renombrar publish arduino

--- Arduino/challenge_final/challenge_final.ino
+++ Arduino/challenge_final/challenge_final.ino
@@ -24,12 +24,12 @@ ros::Subscriber<std_msgs::Float32> input_sub("motor_input", &input_callback);
 std_msgs::Float32 pwm_signal;
 
 //Crear el topico motor_pwm para publicar su valor
-ros::Publisher motor_pwm("motor_pwm", &pwm_signal);
+ros::Publisher motor_output("motor_output", &pwm_signal);
 
 //Iniciamos el nodo, nos subscribimos al topico cmd_pwm e inicializamos los pines del puente H
 void setup() {
   motor.initNode();
-  motor.advertise(motor_pwm);
+  motor.advertise(motor_output);
   pinMode(Enable, OUTPUT);
   pinMode(Izquierda, OUTPUT);
   pinMode(Derecha, OUTPUT);
@@ -53,7 +53,7 @@ void loop() {
   }
 
   //Publicamos el valor del puente HS
-  motor_pwm.publish(&pwm_signal);
+  motor_output.publish(&pwm_signal);
   motor.spinOnce();
   delay(1000);
 }
