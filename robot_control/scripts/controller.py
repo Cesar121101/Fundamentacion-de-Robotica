657efc191e6e6510c8c6f76905044d96b7439bc3
Karen-Rdz <karen.rdz.h@gmail.com>
2023-04-11 11:07:46 -0500

    Intentar que el robot avance solo 2m

--- robot_control/scripts/controller.py
+++ robot_control/scripts/controller.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 import rospy
 import numpy as np
-from robot_control.msg import set_point
 from std_msgs.msg import Float32
 from geometry_msgs.msg import Twist
 
 #Setup parameters, variables and callback functions
-setpoint = set_point()
 out = 0.0
 superError = 0.0
 currentTime = 0.0
@@ -15,6 +13,9 @@ prevError = 0.0
 errorGlob = 0.0
 motorOut = 0.0
 msgRobot = Twist()
+r = 0.05
+l = 0.18
+distance = 2
 
 # PID function 
 def PID(error):
@@ -36,20 +37,10 @@ def PID(error):
 
     # D
     Kd = rospy.get_param("/Kd", "No param found")
-    #D = Kd*((error-prevError)/dt)
-    D = 0
+    D = Kd*((error-prevError)/dt)
     prevError = error
 
     return (P + I + D)
-    
-# This function handles the info inside of the 'set_point' topic
-def set_point_callback(msg):
-    rospy.loginfo("Setpoint: %s", msg.setpoint)
-
-    # The setpoint msg has 2 parts a value (setpoint) and a type (type) which corresponds to either Step, Square and Sine
-    global setpoint
-    setpoint.setpoint = msg.setpoint
-    setpoint.type = msg.type
 
 # This function handles the info inside of the 'motor_output' topic
 def motor_output_callback(msg):
@@ -69,7 +60,6 @@ if __name__=='__main__':
     rospy.on_shutdown(stop)
 
     #Setup Publishers and subscribers here
-    rospy.Subscriber("set_point", set_point, set_point_callback)
     rospy.Subscriber("motor_output", Float32, motor_output_callback)
     input_pub = rospy.Publisher("motor_input", Float32 , queue_size=1)
     error_pub = rospy.Publisher("error", Float32 , queue_size=1) 
@@ -83,35 +73,37 @@ if __name__=='__main__':
         prevTime = currentTime
         currentTime = rospy.get_time()
 
-        error = setpoint.setpoint - motorOut
-        out = PID(error)/30 # 1/30 is our k to account for an amplification
-
-        # Log info about Controller
-        if setpoint.type == 1.0:
-            rospy.loginfo("Type Input: Step")
-        elif setpoint.type == 2.0:
-            rospy.loginfo("Type Input: Square")
-        else:
-            rospy.loginfo("Type Input: Sine")
+        error = distance - msgRobot.linear.x
+        out = PID(error)
 
         # Print to terminal the setpoint, the motor output, the error and the motor input which is the real value that gets sent to the PWM
-        rospy.loginfo("Input Value: %s", setpoint.setpoint)
+        # rospy.loginfo("Input Value: %s", setpoint.setpoint)
         rospy.loginfo("Motor output: %s", motorOut)
         rospy.loginfo("Error: %s", error)
         rospy.loginfo("Motor input: %s", out)
+        rospy.loginfo("Linear x: %s", msgRobot.linear.x)
 
         # Speed regulation to prevent data over 1 or under -1
-        if out > 1.0:
-            out = 1.0
-        elif out < -1.0:
-            out = -1.0
+        # if out > 1.0:
+        #     out = 1.0
+        # elif out < -1.0:
+        #     out = -1.0
 
         # Handle friction and minimal output, for when the motor can't overpower friction
-        if out <= 0.05 and out >= -0.05:
-            out = 0.0
+        # if out <= 0.05 and out >= -0.05:
+        #     out = 0.0
 
-        msgRobot.linear.x = 2
-        msgRobot.angular.z = 1000
+        dt = currentTime-prevTime
+
+        d_sim = msgRobot.linear.x*dt
+        omega_sim = msgRobot.angular.z*dt
+
+        if error > 0:
+            msgRobot.linear.x = out
+        else:
+            msgRobot.linear.x = 0
+       
+        msgRobot.angular.z = 0
 
         # Publish error, motor_input, and velocity for gazebo
         # We handle the error as a topic in order to able to plot it
