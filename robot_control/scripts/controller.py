d03fa539c0998c285b8a4b33f4135c8fd53d3cc0
Cesar Cruz <Cesar121101>
2023-04-12 18:47:51 -0600

    Robot se mueve 2 metros al frente

--- robot_control/scripts/controller.py
+++ robot_control/scripts/controller.py
@@ -12,12 +12,13 @@ prevTime = 0.0
 prevError = 0.0
 errorGlob = 0.0
 motorOut = 0.0
+startTime = 0.0
 msgRobot = Twist()
 r = 0.05
 l = 0.18
 distance = 2
-startTime = 0.0
-isFinished = 0
+difT = 0.0
+notFinish = 1
 
 # PID function 
 def PID(error):
@@ -67,6 +68,11 @@ if __name__=='__main__':
     error_pub = rospy.Publisher("error", Float32 , queue_size=1) 
     cmd_vel = rospy.Publisher("cmd_vel", Twist, queue_size=1)
 
+    #Set initial error
+    error = 2
+
+    msgRobot.linear.x = 0
+    rate.sleep()
     #Set the current time
     startTime = rospy.get_time()
 
@@ -74,36 +80,39 @@ if __name__=='__main__':
     while not rospy.is_shutdown():
         prevTime = currentTime
         currentTime = rospy.get_time()
-
         # error = distance - msgRobot.linear.x
         # out = PID(error)
 
-        dt = currentTime-startTime
+        difT = currentTime-startTime
 
-        d_sim = msgRobot.linear.x*dt
-        omega_sim = r*(msgRobot.angular.z/l)*dt
+        d_sim = (msgRobot.linear.x) * difT
+        omega_sim = r*(msgRobot.angular.z/l)*difT
         error = distance - d_sim
 
-        if error > 0 and isFinished == 0:
+        if error > 0 and notFinish == 1:
             msgRobot.linear.x = 1
-        else:
+        elif error <= 0:
             msgRobot.linear.x = 0
-            isFinished = 1
+            notFinish = 0
 
         #msgRobot.linear.x = out
         msgRobot.angular.z = 0
 
         # Print to terminal the setpoint, the motor output, the error and the motor input which is the real value that gets sent to the PWM
         # rospy.loginfo("Input Value: %s", setpoint.setpoint)
-        rospy.loginfo("Motor output: %s", motorOut)
+        rospy.loginfo("\n")
         rospy.loginfo("Error: %s", error)
-        rospy.loginfo("Motor input: %s", out)
         rospy.loginfo("Linear x: %s", msgRobot.linear.x)
+        rospy.loginfo("Initial time: %s", startTime)
+        rospy.loginfo("Currrent time: %s", currentTime)
+        rospy.loginfo("DT: %s", difT)
+        rospy.loginfo("d_sim: %s", d_sim)
+    
 
         # Speed regulation to prevent data over 1 or under -1
         # if out > 1.0:
         #     out = 1.0
-        # elif out < -1.0:
+        # elif out < -1.0:d_sim
         #     out = -1.0
 
         # Handle friction and minimal output, for when the motor can't overpower friction
