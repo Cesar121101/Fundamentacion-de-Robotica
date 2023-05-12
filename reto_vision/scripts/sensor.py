dcab9ac01bac636f75962f9b09d10c7f1a038380
Alex <alejandro.armidac@gmail.com>
2023-05-11 23:00:50 -0600

    Collisions Work with Owot :)

---
+++ reto_vision/scripts/sensor.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import rospy
+from gazebo_msgs.msg import ContactsState
+
+def callback(data):
+    print("...")
+    # Process the contact sensor data
+    for contact in data.states:
+        print("Collision detected between", contact.collision1_name, "and", contact.collision2_name)
+
+def listener():
+    # Initialize the ROS node
+    rospy.init_node('contact_sensor_listener')
+
+    # Subscribe to the contact sensor topic
+    sub = rospy.Subscriber('/gazebo/default/my_box_1/my_box_link/box_1_contact', ContactsState, callback)
+
+    rate = rospy.Rate(100)
+
+
+    while not rospy.is_shutdown():
+        print("")
+        rate.sleep()
+
+if __name__ == '__main__':
+    listener()
