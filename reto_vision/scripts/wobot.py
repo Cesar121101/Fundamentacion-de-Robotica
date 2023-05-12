7e650b662233461cf3119f5eddde4854e602e706
Alex <alejandro.armidac@gmail.com>
2023-05-12 02:58:51 -0600

    What a piece of junk, ya jala todo mi wobot :) <3

--- reto_vision/scripts/wobot.py
+++ reto_vision/scripts/wobot.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
+# KILL ME.
+# This is one of the most complex and learning heavy scripts I'd have to do
+# Armida
 import rospy
+import sys
 from std_msgs.msg import String
 from trajectory_msgs.msg import JointTrajectory
 from trajectory_msgs.msg import JointTrajectoryPoint
 from gazebo_ros_link_attacher.srv import Attach, AttachRequest, AttachResponse
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from gazebo_msgs.msg import ContactsState
+from tf.transformations import quaternion_from_euler
 
 # rostopic pub 
 # /arm_controller/command 
@@ -14,57 +23,173 @@ recvmsg = ""
 message = JointTrajectory()
 points = JointTrajectoryPoint()
 req = AttachRequest()
+gripper_1_coll_contact = ContactsState()
+gripper_2_coll_contact = ContactsState()
+holding = False
 
+# Grippers configs
+grippers_open_config = [0.27,-0.27]
+grippers_close_config = [0.025,-0.025]
+grippers_closeMagnet_config = [0.040,-0.040]
+
+# Current holding
+current_model = ""
+current_link = ""
+
+# Get the names of the models we collided with
+def get_contacts_names(contact):
+    return [contact.collision1_name.split("::")[0], contact.collision2_name.split("::")[0]]
+
+# Get the link of a contact
+def get_contacts_links(contact):
+    return contact.collision1_name.split("::")[1]
+
+# Check for object collision (anything that touches the 2 gripper hands)
+def check_object(gripper_1_coll_contact, gripper_2_coll_contact, attach_srv):
+    global current_model
+    global current_link
+    global holding
+    if not(holding):
+        for contact1 in gripper_1_coll_contact.states:
+            contacts1 = get_contacts_names(contact1)
+            contact_1_1 = contacts1[0]
+            contact_1_2 = contacts1[1]
+            for contact2 in gripper_2_coll_contact.states:
+                contacts2 = get_contacts_names(contact2)
+                contact_2_1 = contacts2[0]
+                contact_2_2 = contacts2[1]
+
+                print("CHECKING")
+                print(contact_1_1)
+                print(contact_1_2)
+                print(contact_2_1)
+                print(contact_2_2)
+
+                # Check if they are the same
+                if contact_1_1 == contact_2_1:
+                    req.model_name_1 = "robot"
+                    req.link_name_1 = "p3_link"
+                    req.model_name_2 = contact_1_1
+                    req.link_name_2 = get_contacts_links(contact1)
+
+                    current_model = req.model_name_2
+                    current_link = req.link_name_2
+
+                    attach_srv.call(req)
+                    print("HOLDING A CUBE")
+                    return True
+    return False
 
 def activate_callback(msg):
     global recvmsg
     print(msg.data)
     recvmsg = msg.data
 
+# Gripper 1 collided
+def gripper_1_coll_callback(msg):
+    global gripper_1_coll_contact
+    gripper_1_coll_contact = msg
+    #print("Gripper 1 Coll detected")
+
+# Gripper 2 collided
+def gripper_2_coll_callback(msg):
+    global gripper_2_coll_contact
+    gripper_2_coll_contact = msg
+    #print("Gripper 2 Coll detected")
+
 
 if __name__ == '__main__':
-    pub = rospy.Publisher("/gripper/command", JointTrajectory, queue_size=10)
+
+    # Pub to manage the gripper
+    gripper = rospy.Publisher("/gripper/command", JointTrajectory, queue_size=10)
+
+    # Pub to manage the path
+    trajectory = rospy.Publisher("/move_group/display_planned_path", moveit_msgs.msg.DisplayTrajectory, queue_size=20)
+    
+    gripper_1 = rospy.Subscriber('/gripper_1_vals', ContactsState, gripper_1_coll_callback)
+    gripper_2 = rospy.Subscriber('/gripper_2_vals', ContactsState, gripper_2_coll_callback)
+    
+    # This will be input
     sub_wl = rospy.Subscriber("wl", String, activate_callback)
+
+    moveit_commander.roscpp_initialize(sys.argv)
+
     rospy.init_node("wobot")
     rate = rospy.Rate(100)
 
+    # Moveit config
+    robot = moveit_commander.RobotCommander()
+    scene = moveit_commander.PlanningSceneInterface()
+    group_name = "arm"
+    group = moveit_commander.MoveGroupCommander(group_name)
+    group.set_num_planning_attempts(1000)
+    group.set_planning_time(60)
+
+    # Initial position
+    joint_goal = group.get_current_joint_values()
+    joint_goal[0] = 0.174533
+    joint_goal[1] = 0.785398
+    joint_goal[2] = 1.22173
+    joint_goal[3] = 1.5708
+    group.go(joint_goal, wait=True)
+    group.stop()
 
     while not rospy.is_shutdown():
-        print(recvmsg)
+        # Service for magnet
         rospy.wait_for_service('/link_attacher_node/attach')
         rospy.wait_for_service('/link_attacher_node/detach')
-
         attach_srv = rospy.ServiceProxy('/link_attacher_node/attach', Attach)
         detach_srv = rospy.ServiceProxy('/link_attacher_node/detach', Attach)
 
-
+        # Message for closing and opening
         message.joint_names = ["gripper_1","gripper_2"]
 
-        if recvmsg == "a":
-            points.positions = [0.025,-0.025]
+        # Manage a cube picking up
+        if check_object(gripper_1_coll_contact, gripper_2_coll_contact, attach_srv):
+            points.positions = grippers_closeMagnet_config
+            gripper.publish(message)
+            holding = True
 
-        elif recvmsg == "b":
-            points.positions = [0.27,-0.27]
+        if recvmsg.split(",")[0] == "a":
+            points.positions = grippers_close_config
+            gripper.publish(message)
 
-        elif recvmsg == "d":
+        elif recvmsg.split(",")[0] == "b":
+            points.positions = grippers_open_config
+            gripper.publish(message)
+
+        elif recvmsg.split(",")[0] == "d":
             req.model_name_1 = "robot"
             req.link_name_1 = "p3_link"
-            req.model_name_2 = "my_box_1"
-            req.link_name_2 = "my_box_link"
-
-            attach_srv.call(req)
-
-        elif recvmsg == "e":
-            req.model_name_1 = "robot"
-            req.link_name_1 = "p3_link"
-            req.model_name_2 = "my_box_1"
-            req.link_name_2 = "my_box_link"
+            req.model_name_2 = current_model
+            req.link_name_2 = current_link
 
             detach_srv.call(req)
+            holding = False
+
+        # Move to a location
+        elif recvmsg.split(",")[0] == "e":
+            roll = float(recvmsg.split(",")[3])
+            pitch = 0.0
+            yaw = 1.57
+            quaternion = quaternion_from_euler(roll, pitch, yaw)
+            
+            pose_goal = geometry_msgs.msg.Pose()
+            pose_goal.position.x = float(recvmsg.split(",")[1])
+            pose_goal.position.y = 0.0
+            pose_goal.position.z = float(recvmsg.split(",")[2])
+            pose_goal.orientation = geometry_msgs.msg.Quaternion(*quaternion)
+            group.set_pose_target(pose_goal)
+            plan = group.go(wait=True)
+            group.stop()
+            #group.clear_pose_targets()
+
+            print("SUCCESS")
 
         elif recvmsg == "f":
             # Do nothing
-            print("...")
+            #print("...")
+            a = 2
 
         else:
             points.positions = [0.025,-0.025]
@@ -72,5 +197,4 @@ if __name__ == '__main__':
         points.time_from_start = rospy.Duration(2.0)
         message.points = [points]
         recvmsg = "f"
-        pub.publish(message)
         rate.sleep()
