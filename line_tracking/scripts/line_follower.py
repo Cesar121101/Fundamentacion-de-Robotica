4bac36fb08a4331cf00f15e7e337ebf29f607683
Cesar121101 <A00829832@itesm.mx>
2023-05-26 19:14:15 -0600

    cambios con el line follower

--- line_tracking/scripts/line_follower.py
+++ line_tracking/scripts/line_follower.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.7
+#!/usr/bin/env python2.7
 import rospy
 import cv2
 import numpy as np
@@ -16,7 +16,6 @@ from std_msgs.msg import String
 # export ROS_IP=10.42.0.16
 # export ROS_MASTER_URI=http://10.42.0.1:11311
 # scp line_follower.py puzzlebot@10.42.0.1:/home/puzzlebot/catkin_ws/src/line_tracking/scripts
-
 
 instruction = ""
 
@@ -44,11 +43,10 @@ def camera_callback(msg):
 
     # Apply adaptive threshold to obtain a binary image
     gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
-    binary = cv2.adaptiveThreshold(binary, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 4)
+    binary = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 4)
 
     # Apply Canny Edge Detector
     edges = cv2.Canny(binary, 100, 100)
-
 
     # Create a black mask with the same dimensions as the edges image
     mask = np.zeros_like(edges)
@@ -65,171 +63,241 @@ def camera_callback(msg):
 
     # Draw area of interest
     cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+    # # Find the contours (Jetson)
+    # contours, _= cv2.findContours(masked_edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # # Find the contours
+    # # _, contours, hierarchy = cv2.findContours(masked_edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # widest_line_width = 0
+    # highest_line_height = 0
+    # widest_contours = []
+    # contour_size = []
+    # sec_filter = []
     
-    # contour = np.array([[x1, y1], [x2, y1], [x2, y2], [x1, y2]])
-    # moments = cv2.moments(contour)
-    # centroid_x = int(moments['m10'] / moments['m00'])
-    # centroid_y = int(moments['m01'] / moments['m00'])
-    # cv2.circle(image, (centroid_x, centroid_y), 5, (255, 0, 255), -1)
+    # #Find the widthest and longest contour
+    # for contour in contours:
+    #     x, y, w, h = cv2.boundingRect(contour)
+
+    #     # Calculate line width and height
+    #     line_width = w
+    #     line_height = h
+
+    #     # Check if the contour meets the criteria
+    #     if line_height > highest_line_height:
+    #         widest_line_width = line_width
+    #         highest_line_height = line_height
     
-    # Find the contours (Jetson)
-    contours, _= cv2.findContours(masked_edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # # print("Ancho: " + str(widest_line_width))
+    # # print("Alto: " + str(highest_line_height))
 
-    # Find the contours
-    # _, contours, hierarchy = cv2.findContours(masked_edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # # # Draw all widest line contours
+    # # cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
 
-    widest_line_width = 0
-    highest_line_height = 0
-    widest_contours = []
-    contour_size = []
-    sec_filter = []
-    cont = 0
+    # #Find the contours similar to the biggest contour
+    # for contour in contours:
+    #     x, y, w, h = cv2.boundingRect(contour)
 
-    #Find the widthest and longest contour
-    for contour in contours:
-        x, y, w, h = cv2.boundingRect(contour)
-
-        # Calculate line width and height
-        line_width = w
-        line_height = h
-
-        # Check if the contour meets the criteria
-        if line_width > widest_line_width and line_height > highest_line_height:
-            widest_line_width = line_width
-            highest_line_height = line_height
-    
-    # print("Ancho: " + str(widest_line_width))
-    # print("Alto: " + str(highest_line_height))
-
-    #Find the contours similar to the biggest contour
-    for contour in contours:
-        x, y, w, h = cv2.boundingRect(contour)
-
-        # Calculate line width and height
-        line_width = w
-        line_height = h
+    #     # Calculate line width and height
+    #     line_width = w
+    #     line_height = h
         
-        # Collect all contours that meet the criteria
-        if ((line_width >= widest_line_width * 0.2 and line_height >= highest_line_height * 0.85) or (line_width == widest_line_width and line_height == highest_line_height)):
-            # Calcula los momentos del contorno
-            moments = cv2.moments(contour)
-            if(moments['m00'] > 0):
-                # print(str(line_width) + " " + str(line_height))
-                # Calcula los centroides del contorno
-                centroid_x = int(moments['m10'] / moments['m00'])
-                centroid_y = int(moments['m01'] / moments['m00'])
-                
-                # Guarda las coordenadas del contorno y su centroide
-                contour_size.append([x, y, centroid_x, centroid_y, w, h])
-                widest_contours.append(contour)
-    
-    # #Find index of the widesth line
-    # for i in contour_size:
-    #     if(i[4]  == widest_line_width):
-    #         index = cont
-    #         break
-    #     else:
-    #         cont+=1
-    
+    #     # Collect all contours that meet the criteria
+    #     if ((line_width >= widest_line_width * 0.05 and line_height >= highest_line_height * 0.7) or (line_width == widest_line_width and line_height == highest_line_height)):
+            
+    #         centroid_x = (x + w/2) 
+    #         centroid_y = (y + h/2)
+
+    #         # Guarda las coordenadas del contorno y su centroide
+    #         contour_size.append([x, y, centroid_x, centroid_y, w, h])
+    #         widest_contours.append(contour)
+
+    #         # Calcula los momentos del contorno
+    #         # moments = cv2.moments(contour)
+    #         # print(moments)
+    #         # if(moments['m00'] > 0):
+    #         #     # print(str(line_width) + " " + str(line_height))
+    #         #     # Calcula los centroides del contorno
+    #         #     centroid_x = int(moments['m10'] / moments['m00'])
+    #         #     centroid_y = int(moments['m01'] / moments['m00'])
+
+    # delete_index = []
+    # index2 = 0
+
+    # # print(contour_size)
+
+    # for i in contour_size: 
+    #     index = contour_size.index(i)
+    #     if((index in delete_index) != True):
+    #         for j in contour_size:
+    #             index2 = contour_size.index(j)
+    #             if((index2 in delete_index) != True):
+    #                 # print("I: " + str(i) + "J: " + str(j))
+    #                 if(i != j):
+    #                     if(abs(i[2] - j[2]) > 40):
+    #                         if((i in sec_filter) != True):
+    #                             if(i[5] > j[5]):
+    #                                 if ((i in sec_filter) != True): 
+    #                                     sec_filter.append(i)
+    #                                     # print("Agregar I")
+    #                             else:
+    #                                 if ((j in sec_filter) != True):
+    #                                     sec_filter.append(j)
+    #                                     # print("Agregar J") 
+    #                         else:
+    #                             if((j in sec_filter) != True):
+    #                                 sec_filter.append(j)
+    #                                 # print("Agregar J") 
+    #                     else:
+    #                         if(i[5] > j[5]):
+    #                             # print("Borrar J")
+    #                             try: 
+    #                                 index = contour_size.index(j)
+    #                                 delete_index.append(index)
+    #                             except Exception as e:
+    #                                 index = 0
+    #                             try: 
+    #                                 index = sec_filter.index(j)
+    #                                 sec_filter.pop(index)
+    #                             except Exception as e:
+    #                                 index = 0
+    #                         else: 
+    #                             # print("Borrar I")
+    #                             try:
+    #                                 index = contour_size.index(i)
+    #                                 delete_index.append(index)
+    #                             except Exception as e:
+    #                                 index = 0
+    #                             try: 
+    #                                 index = sec_filter.index(i)
+    #                                 sec_filter.pop(index)
+    #                             except Exception as e:
+    #                                 index = 0
+    #     # print(delete_index)
     # cont = 0
 
-    # sec_filter.append(contour_size[index])
+    # for i in delete_index:       
+    #     contour_size.pop(i-cont)
+    #     widest_contours.pop(i-cont)
+    #     cont += 1
+        
+    # # print(sec_filter)
+    # # print(contour_size)
 
-    for i in contour_size: 
-        for j in contour_size:
-            if(i != j):
-                print("I: " + str(i) + "J: " + str(j))
-                if(abs(i[2] - j[2]) > 50):
-                    sec_filter.append(j)
-                else:
-                    try: 
-                        index = sec_filter.index(j)
-                        sec_filter.pop(index)
-                    except Exception as e:
-                        index = 0
-                    contour_size.pop(cont)
-                    widest_contours.pop(cont)
-            cont+=1
-        cont = 0
+    # highest = 0
+    # highest2 = 0
+    # cont = 0
 
-    print("Contornos: "+ str(len(sec_filter)))
-    print("Len: " + str(len(widest_contours)))
+    # #Finda largest
+    # for i in sec_filter:
+    #     if(i[5] > highest2): 
+    #         if(i[5] > highest):
+    #             highest = i[5]
+    #         else: 
+    #             highest2 = i[5]
 
-    # print(sec_filter)
-    # print("C: " + str(sec_filter))
-    for c in sec_filter: 
-        cv2.rectangle(image, (c[0],c[1]), (c[0]+c[4], c[1]+c[5]), (255, 255, 255), 2)
-        cv2.circle(image, (c[2], c[3]), 5, (0, 255, 255), -1)
+    # for i in sec_filter: 
+    #     if(i[5] == highest or i[5] == highest2):
+    #         cont+=1
+    #     else: 
+    #         sec_filter.pop(cont)
+    #         contour_size.pop(cont)
+    #         widest_contours.pop(cont)
+    #         if(len(sec_filter) == 2):
+    #             break
 
-    # Draw all widest line contours
-    cv2.drawContours(image, widest_contours, -1, (0, 255, 0), 2)
+    # print("Contornos F: "+ str(len(sec_filter)))
+    # # print("Contornos I: " + str(len(contour_size)))
+    # # print("Iniciales: " + str(len(widest_contours)))
 
-    # #Center of the line
-    center_xl = (contour_size[0][2] + contour_size[1][2])/2
-    center_yl = (contour_size[0][3] + contour_size[1][3])/2
+    # # print(sec_filter)
+    # # print("C: " + str(sec_filter))
+    # for c in sec_filter: 
+    #     cv2.rectangle(image, (c[0],c[1]), (c[0]+c[4], c[1]+c[5]), (255, 255, 255), 2)
+    #     cv2.circle(image, (int(c[2]), int(c[3])), 5, (0, 255, 255), -1)
+
+    # # Draw all widest line contours
+    # cv2.drawContours(image, widest_contours, -1, (0, 255, 0), 2)
+    # if(len(sec_filter) == 2):
+    #     # Center of the line
+    #     center_xl = (sec_filter[0][2] + sec_filter[1][2])/2
+    #     center_yl = (sec_filter[0][3] + sec_filter[1][3])/2
+    # else: 
+    #     instruction = "stop"
     
-    # Obtiene las dimensiones de la imagen
+    # # Obtiene las dimensiones de la imagen
+    # height, width, _ = image.shape
+
+    # # Calcula las coordenadas del centro
+    # center_x = int(width / 2)
+
+    # # cv2.circle(image, (int(center_xl), int(center_yl)), 5, (255, 0, 255), -1)
+    # # cv2.circle(image, (int(center_x), int(center_yl)), 5, (255, 255, 0), -1)
+
+    # if(abs(center_xl - center_x) < 55):
+    #     instruction = "forward"
+    # elif(center_xl - center_x > 55): 
+    #     instruction = "right"
+    # elif(center_xl - center_x < 55): 
+    #     instruction = "left"
+    # else: 
+    #     instruction = "stop"
+
+    # Left rectangle
+    # Obtener las dimensiones de la imagen
     height, width, _ = image.shape
 
-    # Calcula las coordenadas del centro
-    center_x = int(width / 2)
+    # Definir las coordenadas de la region central
+    x1_centro = int(width / 2 - 10)
+    x2_centro = int(width / 2 + 10)
+    y1_centro = int(height / 2 - 250)
+    y2_centro = int(height/ 2 - 225)
 
-    cv2.circle(image, (int(center_xl), int(center_yl)), 5, (255, 0, 255), -1)
-    cv2.circle(image, (int(center_x), int(center_yl)), 5, (255, 255, 0), -1)
+    # Dibujar un rectangulo para visualizar la region central
+    cv2.rectangle(image, (x1_centro, y1_centro), (x2_centro, y2_centro), (255, 0, 0), 2)
 
-    if(abs(center_xl - center_x) < 55):
-        instruction = "forward"
-    elif(center_xl - center_x > 55): 
-        instruction = "right"
-    elif(center_xl - center_x < 55): 
-        instruction = "left"
-    else: 
+    # Obtener la region central de la imagen
+    roi_centro = image[y1_centro:y2_centro, x1_centro:x2_centro]
+
+    # Calcular el color promedio de la region central
+    color_promedio_centro = np.mean(roi_centro, axis=(0, 1))
+    color_promedio_rgb_centro = cv2.cvtColor(np.uint8([[color_promedio_centro]]), cv2.COLOR_BGR2RGB)
+
+    # Imprimir el color promedio
+    print(color_promedio_rgb_centro.mean())
+    
+    if(color_promedio_rgb_centro.mean() > 120):
         instruction = "stop"
+    else:
+        instruction = "forward"
 
-    # # Left rectangle
-    # x1_r1, x2_r1, y1_r1, y2_r1 = 475, 525, 650, 700
-    # cv2.rectangle(image, (x1_r1,y1_r1), (x2_r1, y2_r1), (255, 0, 0), 2)
-    # roi1 = image[y1_r1:y2_r1, x1_r1:x2_r1]
-    # color_promedio1 = np.mean(roi1, axis=(0, 1))
-    # color_promedio_rgb1 = cv2.cvtColor(np.uint8([[color_promedio1]]), cv2.COLOR_BGR2RGB)
-    # if(color_promedio_rgb1.mean() > 127):
+    # if(color_promedio_rgb1.mean() > 20):
     #     color_promedio_rgb1 = [255, 255, 255]
     # else: 
     #     color_promedio_rgb1 = [0, 0, 0]
 
     # # Right rectangle
-    # x1_r2, x2_r2, y1_r2, y2_r2 = 725, 775, 650, 700
+    # x1_r2, x2_r2, y1_r2, y2_r2 = 620, 670, 650, 700
     # cv2.rectangle(image, (x1_r2,y1_r2), (x2_r2, y2_r2), (0, 255, 0), 2)
     # roi2 = image[y1_r2:y2_r2, x1_r2:x2_r2]
-    # color_promedio2 = np.mean(roi2, axis=(0, 1))
+    # color_promedio2 = np.mean(roi2)
     # color_promedio_rgb2 = cv2.cvtColor(np.uint8([[color_promedio2]]), cv2.COLOR_BGR2RGB)
-    # if(color_promedio_rgb2.mean() > 127):
+    # print(color_promedio_rgb2.mean())
+    # if(color_promedio_rgb2.mean() > 20):
     #     color_promedio_rgb2 = [255, 255, 255]
     # else: 
     #     color_promedio_rgb2 = [0, 0, 0]
 
-
-    # # Middle rectangle
-    # x1_r3, x2_r3, y1_r3, y2_r3 = 600, 650, 650, 700
-    # cv2.rectangle(image, (x1_r3,y1_r3), (x2_r3, y2_r3), (0, 0, 255), 2)
-    # roi3 = image[y1_r3:y2_r3, x1_r3:x2_r3]
-    # color_promedio3 = np.mean(roi3, axis=(0, 1))
-    # color_promedio_rgb3 = cv2.cvtColor(np.uint8([[color_promedio3]]), cv2.COLOR_BGR2RGB)
-    # if(color_promedio_rgb3.mean() > 127):
-    #     color_promedio_rgb3 = [255, 255, 255]
-    # else: 
-    #     color_promedio_rgb3 = [0, 0, 0]
-
     # print("Color promedio 1: ", color_promedio_rgb1)
     # print("Color promedio 2: ", color_promedio_rgb2)
-    # print("Color promedio 3: ", color_promedio_rgb3)
 
     # #Continue forward
-    # if(color_promedio_rgb1 == color_promedio_rgb2 and color_promedio_rgb1 != color_promedio_rgb3):
+    # if(color_promedio_rgb1 == color_promedio_rgb2 and color_promedio_rgb1 == [0, 0, 0]):
     #     instruction = "forward"
-    # elif(color_promedio_rgb2 == color_promedio_rgb3 and color_promedio_rgb3 != color_promedio_rgb1): #Turn left
+    # elif(color_promedio_rgb2 == [255, 255, 255]): #Turn left
     #     instruction = "left"
-    # elif(color_promedio_rgb1 == color_promedio_rgb3 and color_promedio_rgb3 != color_promedio_rgb2): #Turn right
+    # elif(color_promedio_rgb1 == [255, 255, 255]): #Turn right
     #     instruction = "right"
     # else: 
     #     instruction = "stop"
@@ -261,7 +329,7 @@ if __name__=='__main__':
     # camera_callback()
 
 
-    #Run the node
+       #Run the node
     while not rospy.is_shutdown():
         instructor_pub.publish(instruction)
         rate.sleep()
